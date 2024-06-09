a75b1ea20d27352d9e2ee3801d4b1dd85d9d46a4
yudai hiyama <yudai1204h@gmail.com>
2024-06-09 17:39:05 +0900

    llamaIndexいい感じ

---
+++ genIndex.py
@@ -0,0 +1,32 @@
+from llama_index.core import (
+    Settings,
+    SimpleDirectoryReader,
+    VectorStoreIndex,
+    SimpleDirectoryReader,
+    VectorStoreIndex,
+)
+from llama_index.embeddings.gemini import GeminiEmbedding
+import logging
+import sys
+import logging
+import sys
+
+USE_GEMINI = False
+
+# ログレベルの設定
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, force=True)
+
+# ドキュメントの読み込み
+documents = SimpleDirectoryReader("data").load_data()
+
+if USE_GEMINI:
+    # 埋め込みモデルの準備
+    Settings.embed_model = GeminiEmbedding(
+        model_name="models/text-embedding-004",
+    )
+
+# インデックスの作成
+index = VectorStoreIndex.from_documents(documents)
+
+# インデックスの保存
+index.storage_context.persist()
